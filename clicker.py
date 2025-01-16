1f4f4542f047f9c0815fc6a22f8e34d2522be653
jonaswalker <jonaswalker99@yahoo.com>
2025-01-15 21:29:45 -0600

    update clicker again

--- clicker.py
+++ clicker.py
@@ -5,29 +5,9 @@ import time
 from tkinter import *
 import pyautogui
 
-pyautogui.click()
-
-# Change the working directory to the folder this script is in.
-# Doing this because I'll be putting the files from each video in their own folder on GitHub
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-# should be center of guy
-# x,y = 900, 490
-
 root = Tk()
 root.title("OSRS Bot")
 root.geometry("500x300")
-
-# initialize the WindowCapture class
-wincap = WindowCapture('RuneLite - Its xMythic2')
-# initialize the Vision class
-vision_thing = Vision('fishingSpot.jpg')
-
-# vision_thing = Vision('fullInventoryOfCookedFish.jpg')
-
-# start fishing process
-# assume inventory is empty (exept rod and feathers)
-# assume inventory is closed---
 
 
 def clicker():
